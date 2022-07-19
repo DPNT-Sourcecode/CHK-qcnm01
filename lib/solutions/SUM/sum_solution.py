767e3e81a1a34a9f19b53b4a08e6130ea122fbc9
sbx_user1051 <sbx_user1051@169.254.138.221>
2022-07-19 09:17:32 +0000

    Tue Jul 19 09:17:32 UTC 2022

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,3 +1,7 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x, y):
-    raise NotImplementedError()
+    if 0 <= x <= 100 and 0 <= y <= 100:
+        return x + y
+    else:
+        raise Exception("Error: x and y are not within the authorized range.")
+
