1fd1bb58f98e529682fad793e362073b1b88ffed
sbx_user1051 <sbx_user1051@169.254.138.221>
2022-07-19 10:07:32 +0000

    Tue Jul 19 10:07:32 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -13,7 +13,6 @@ def checkout(skus):
         | E    | 40    | 2E get one B free      |
         +------+-------+------------------------+
     """
-    print(f"skus are: {skus}")
     table_offer = {'A': 50, 'B': 30, 'C': 20, 'D': 15, 'E': 40}
     checkout = {'A': 0, 'B': 0, 'C': 0, 'D': 0, 'E': 0}
     total = 0
@@ -25,6 +24,10 @@ def checkout(skus):
             return -1
     checkout['B'] -= checkout['E'] // 2  # Every 2E bought, we remove a B from the checkout (= free)
 
+    # In case there is less B in our basket than we can get for free
+    if checkout['B'] < 0:
+        checkout['B'] = 0
+
     total += checkout['A'] // 5 * 200 + checkout['A'] % 5 // 3 * 130 + checkout['A'] % 5 % 3 * 50 +\
              checkout['B'] // 2 * 45 + checkout['B'] % 2 * 30
     total += sum([checkout[sku] * table_offer[sku] for sku in 'CDE'])
@@ -32,6 +35,7 @@ def checkout(skus):
     print(f"Total of {skus}: {total}")
 
     return total
+
 
 
 
