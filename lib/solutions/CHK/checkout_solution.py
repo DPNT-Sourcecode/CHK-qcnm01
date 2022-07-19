3b1eee6848762ac2a93ccda06cd3618532bf551a
sbx_user1051 <sbx_user1051@169.254.138.221>
2022-07-19 10:00:24 +0000

    Tue Jul 19 10:00:24 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,15 +3,17 @@
 # skus = unicode string
 def checkout(skus):
     """
-    +------+-------+----------------+
-    | Item | Price | Special offers |
-    +------+-------+----------------+
-    | A    | 50    | 3A for 130     |
-    | B    | 30    | 2B for 45      |
-    | C    | 20    |                |
-    | D    | 15    |                |
-    +------+-------+----------------+
+        +------+-------+------------------------+
+        | Item | Price | Special offers         |
+        +------+-------+------------------------+
+        | A    | 50    | 3A for 130, 5A for 200 |
+        | B    | 30    | 2B for 45              |
+        | C    | 20    |                        |
+        | D    | 15    |                        |
+        | E    | 40    | 2E get one B free      |
+        +------+-------+------------------------+
     """
+    print(f"skus are: {skus}")
     table_offer = {'A': 50, 'B': 30, 'C': 20, 'D': 15}
     checkout = {'A': 0, 'B': 0, 'C': 0, 'D': 0}
     total = 0
@@ -21,9 +23,12 @@ def checkout(skus):
             checkout[char] += 1
         else:
             return -1
-    total += checkout['A'] // 3 * 130 + checkout['A'] % 3 * 50 +\
+    checkout['B'] -= checkout['E'] // 2  # Every 2E bought, we remove a B from the checkout (= free)
+
+    total += checkout['A'] // 5 * 200 + checkout['A'] // 3 * 130 + checkout['A'] % 3 * 50 +\
              checkout['B'] // 2 * 45 + checkout['B'] % 2 * 30
     total += checkout['C'] * 20 + checkout['D'] * 15
 
     return total
+
 
